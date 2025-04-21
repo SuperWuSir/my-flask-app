c8db6dce2b85f9b43a1186db4c8abed79dd3d73b
unknown <a989638800@gmail.com>
2025-04-21 15:43:18 +0800

    HomeBtn

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def get_counties(state_code):
         return jsonify(json.load(f))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
